e22fa91d9e5f19ca00ad3d3cafb52d79707037ee
EwelinaGordon <ewelina.gordon@gmail.com>
2018-05-15 19:33:21 +0200

    Dodanie .gitignore

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,7 @@ class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Ucze sie Javy!");
         System.out.println("Kontrola!");
+        System.out.println("Sprawdzenie2");
 
 
     }
